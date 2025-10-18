de275eae21d84e5b0501bc91b529d67f6ee0353e
Sabiqismail <sabikpp@gmail.com>
2025-10-19 01:32:22 +0400

    Removed deprecated property kotlin.incremental.useClasspathSnapshot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,13 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
     plugins {
         id("com.android.application") version "8.7.2"
         id("org.jetbrains.kotlin.android") version "2.0.21"
         id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
-        id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
     }
 }
 
